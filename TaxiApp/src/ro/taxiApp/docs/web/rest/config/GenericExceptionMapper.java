8feb6b67dff5f7e1a4f4067d4c3b9ceb48d69c83
Ursu Denis <denis.ursu@uab.ro>
2021-05-18 00:21:23 +0300

    backend cleanup 6

---
+++ TaxiApp/src/ro/taxiApp/docs/web/rest/config/GenericExceptionMapper.java
@@ -0,0 +1,34 @@
+package ro.taxiApp.docs.web.rest.config;
+
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+import org.apache.commons.lang.StringUtils;
+
+import ro.taxiApp.docs.core.AppExceptionCodes;
+import ro.taxiApp.docs.presentation.client.shared.model.ErrorMessageModel;
+
+@Provider
+public class GenericExceptionMapper implements ExceptionMapper<Throwable> {
+
+	@Override
+	public Response toResponse(Throwable t) {
+		int statusCode = Status.INTERNAL_SERVER_ERROR.getStatusCode();
+		if (t instanceof WebApplicationException) {
+			statusCode = ((WebApplicationException) t).getResponse().getStatus();
+		}
+		String errorDetails = t.getMessage();
+		if (StringUtils.isBlank(errorDetails)) {
+			errorDetails = Status.INTERNAL_SERVER_ERROR.getReasonPhrase();
+		}
+		return Response.status(statusCode)
+				.entity(new ErrorMessageModel(AppExceptionCodes.APPLICATION_ERROR.name(), errorDetails))
+				.type(MediaType.APPLICATION_JSON)
+				.build();
+	}
+	
+}
---
+++ TaxiApp/src/ro/taxiApp/docs/web/rest/config/PresentationExceptionMapper.java
@@ -0,0 +1,35 @@
+package ro.taxiApp.docs.web.rest.config;
+
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+import org.apache.commons.lang.StringUtils;
+
+import ro.taxiApp.docs.core.AppExceptionCodes;
+import ro.taxiApp.docs.presentation.client.shared.exceptions.PresentationException;
+import ro.taxiApp.docs.presentation.client.shared.model.ErrorMessageModel;
+
+@Provider
+public class PresentationExceptionMapper implements ExceptionMapper<PresentationException> {
+
+	@Override
+	public Response toResponse(PresentationException pe) {
+		int status = Status.INTERNAL_SERVER_ERROR.getStatusCode();
+		String errorCode = pe.getCode();
+		if (StringUtils.isBlank(errorCode)) {
+			errorCode = AppExceptionCodes.APPLICATION_ERROR.name();
+		}
+		String errorDetails = pe.getMessage();
+		if (StringUtils.isBlank(errorDetails)) {
+			errorDetails = Status.INTERNAL_SERVER_ERROR.getReasonPhrase();
+		}
+		return Response.status(status)
+				.entity(new ErrorMessageModel(errorCode, errorDetails))
+				.type(MediaType.APPLICATION_JSON)
+				.build();
+	}
+
+}
